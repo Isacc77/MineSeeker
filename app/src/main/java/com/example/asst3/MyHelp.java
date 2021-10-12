2f48b171876f8e62e31e7b6d141580938b4ef7b7
26973 <shuai_li_4@sfu.ca>
2021-10-12 00:56:58 -0700

    Welcome screen finished.
    find some images for future work

---
+++ app/src/main/java/com/example/asst3/MyHelp.java
@@ -0,0 +1,23 @@
+package com.example.asst3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyHelp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_help);
+
+
+
+
+
+
+
+
+
+    }
+}
