54a1272b375ac7804a587df3dc66f1dc43587474
26973 <shuai_li_4@sfu.ca>
2021-10-17 03:27:10 -0700

    doing game class
    when user tap btn, the row of that btn and the col of that btn need to response

---
+++ app/src/main/java/com/example/asst3/model/Mine.java
@@ -0,0 +1,29 @@
+package com.example.asst3.model;
+
+public class Mine {
+
+
+    private boolean isMine = false;
+
+
+
+    public Mine(boolean isMine) {
+        this.isMine = isMine;
+    }
+
+
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
+
+
+
+
+
+}
