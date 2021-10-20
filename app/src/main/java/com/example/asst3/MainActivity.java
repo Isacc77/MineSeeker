af45e80c1f2fb156f1dff0ce067d7b25c4e3d04f
sla333 <shuai_li_4@sfu.ca>
2021-10-20 01:41:25 +0000

    Update MainActivity.java

--- app/src/main/java/com/example/asst3/MainActivity.java
+++ app/src/main/java/com/example/asst3/MainActivity.java
@@ -2,20 +2,14 @@ package com.example.asst3;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.asst3.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
